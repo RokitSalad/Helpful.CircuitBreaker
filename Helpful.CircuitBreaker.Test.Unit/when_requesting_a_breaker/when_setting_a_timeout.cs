9f0a57ba0a41df1157d779fc4d14eb02e1286228
RokitSalad <peterdobbs@hotmail.com>
2014-08-06 17:16:31 +0100

    Adding first unit test

---
+++ Helpful.CircuitBreaker.Test.Unit/when_requesting_a_breaker/when_setting_a_timeout.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Helpful.BDD;
+using Helpful.CircuitBreaker;
+using Helpful.CircuitBreaker.Events;
+using Moq;
+
+namespace when_requesting_a_breaker
+{
+    class when_setting_a_timeout : TestBase
+    {
+        private Mock<IEventFactory> _eventFactory;
+        private CircuitBreakerFactory _factory;
+
+        protected override void Given()
+        {
+            _eventFactory = new Mock<IEventFactory>();
+            _factory = new CircuitBreakerFactory(_eventFactory.Object);
+        }
+
+        protected override void When()
+        {
+            
+        }
+
+        [Then]
+        public void the_timeout_is_set()
+        {
+            
+        }
+    }
+}
