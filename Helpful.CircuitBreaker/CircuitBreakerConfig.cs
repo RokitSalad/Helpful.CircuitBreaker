d5135726920a6fb4c625898b8e382f9a18bd193b
RokitSalad <peterdobbs@hotmail.com>
2014-08-07 11:59:17 +0100

    Added some more tests

--- Helpful.CircuitBreaker/CircuitBreakerConfig.cs
+++ Helpful.CircuitBreaker/CircuitBreakerConfig.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 
 namespace Helpful.CircuitBreaker
 {
+    [Serializable]
     public class CircuitBreakerConfig
     {
         public TimeSpan Timeout { get; set; }
         public bool UseTimeout { get; set; }
         public bool UseExceptionWhiteList { get; set; }
         public bool UseExceptionBlackList { get; set; }
-        public IList<Type> ExceptionWhiteList { get; set; }
-        public IList<Type> ExceptionBlackList { get; set; }
+        public List<Type> ExceptionWhiteList { get; set; }
+        public List<Type> ExceptionBlackList { get; set; }
         public short OpenEventTollerance { get; set; }
     }
 }
