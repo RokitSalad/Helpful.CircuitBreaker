00f221874372810f0e01f04fb1196586d051befd
RokitSalad <peterdobbs@hotmail.com>
2014-08-30 17:01:53 +0100

    Added tests confirming black and white list exception pass through with tolerance

---
+++ Helpful.CircuitBreaker.Test.Unit/when_executing_code_via_the_breaker/when_tolerating_open_events/when_receiving_exceptions_not_in_the_blacklist_within_tolerance.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using Helpful.BDD;
+using Helpful.CircuitBreaker;
+using Helpful.CircuitBreaker.Exceptions;
+using Helpful.CircuitBreaker.Test.Unit;
+using Moq;
+using NUnit.Framework;
+
+namespace when_executing_code_via_the_breaker.when_tolerating_open_events
+{
+    class when_receiving_exceptions_not_in_the_blacklist_within_tolerance : using_a_mocked_event_factory
+    {
+        private CircuitBreakerConfig _config;
+        private CircuitBreaker _circuitBreaker;
+        private List<Exception> _caughtExceptions;
+        private NullReferenceException _thrownException;
+
+        protected override void Given()
+        {
+            base.Given();
+            _caughtExceptions = new List<Exception>();
+            _config = new CircuitBreakerConfig { ExpectedExceptionListType = ExceptionListType.BlackList };
+            _config.ExpectedExceptionList.Add(typeof(ArgumentNullException));
+            _circuitBreaker = Factory.GetBreaker(_config);
+            _thrownException = new NullReferenceException();
+        }
+
+        protected override void When()
+        {
+            CallExecute();
+            CallExecute();
+        }
+
+        private void CallExecute()
+        {
+            try
+            {
+                _circuitBreaker.Execute(() => { throw _thrownException; });
+            }
+            catch (Exception e)
+            {
+                _caughtExceptions.Add(e);
+            }
+        }
+
+        [Then]
+        public void an_exception_should_be_thrown_for_each_call()
+        {
+            Assert.That(_caughtExceptions.Count, Is.EqualTo(2));
+        }
+
+        [Then]
+        public void no_exceptions_should_be_breaker_open_exceptions()
+        {
+            Assert.That(_caughtExceptions, Has.No.TypeOf<CircuitBreakerOpenException>());
+        }
+
+        [Then]
+        public void the_caught_exceptions_should_be_the_thrown_exceptions()
+        {
+            Assert.That(_caughtExceptions[0], Is.TypeOf<NullReferenceException>());
+            Assert.That(_caughtExceptions[1], Is.TypeOf<NullReferenceException>());
+        }
+
+        [Then]
+        public void the_breaker_should_remain_closed()
+        {
+            Assert.That(_circuitBreaker.State, Is.EqualTo(BreakerState.Closed));
+        }
+
+        [Then]
+        public void no_tollerated_open_events_should_be_raised()
+        {
+            TolleratedOpenEvent.Verify(e => e.RaiseEvent(It.IsAny<short>(), _config, BreakerOpenReason.Exception, It.IsAny<Exception>()), Times.Never);
+        }
+    }
+}
