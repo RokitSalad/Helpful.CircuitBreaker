cfd202499f44a05d9b3572eeac03aee17e6a8571
RokitSalad <peterdobbs@hotmail.com>
2014-08-30 16:50:47 +0100

    Added tests for exceptions within timeouts being thrown either side of a tolerance setting

---
+++ Helpful.CircuitBreaker.Test.Unit/when_executing_code_via_the_breaker/when_tolerating_open_events/when_receiving_exceptions_before_hitting_timeouts_beyond_tolerance.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using Helpful.BDD;
+using Helpful.CircuitBreaker;
+using Helpful.CircuitBreaker.Exceptions;
+using Helpful.CircuitBreaker.Test.Unit;
+using Moq;
+using NUnit.Framework;
+
+namespace when_executing_code_via_the_breaker.when_tolerating_open_events
+{
+    class when_receiving_exceptions_before_hitting_timeouts_beyond_tolerance : using_a_mocked_event_factory
+    {
+        private CircuitBreakerConfig _config;
+        private CircuitBreaker _circuitBreaker;
+        private List<Exception> _caughtExceptions;
+        private ArgumentNullException _thrownException;
+
+        protected override void Given()
+        {
+            base.Given();
+            _caughtExceptions = new List<Exception>();
+            _thrownException = new ArgumentNullException();
+            _config = new CircuitBreakerConfig
+            {
+                Timeout = TimeSpan.FromMilliseconds(1000),
+                UseTimeout = true,
+                OpenEventTolerance = 2,
+                RetryPeriodInSeconds = 60
+            };
+            _circuitBreaker = Factory.GetBreaker(_config);
+        }
+
+        protected override void When()
+        {
+            CallExecute();
+            CallExecute();
+            CallExecute();
+            CallExecute();
+        }
+
+        private void CallExecute()
+        {
+            try
+            {
+                _circuitBreaker.Execute(() => { throw _thrownException; });
+            }
+            catch (Exception e)
+            {
+                _caughtExceptions.Add(e);
+            }
+        }
+
+        [Then]
+        public void an_exception_should_be_thrown_for_each_call()
+        {
+            Assert.That(_caughtExceptions.Count, Is.EqualTo(4));
+        }
+
+        [Then]
+        public void the_first_three_exceptions_should_be_execution_errors()
+        {
+            Assert.That(_caughtExceptions[0], Is.TypeOf<CircuitBreakerExecutionErrorException>());
+            Assert.That(_caughtExceptions[1], Is.TypeOf<CircuitBreakerExecutionErrorException>());
+            Assert.That(_caughtExceptions[2], Is.TypeOf<CircuitBreakerExecutionErrorException>());
+        }
+
+        [Then]
+        public void the_fourth_exception_should_be_breaker_open()
+        {
+            Assert.That(_caughtExceptions[3], Is.TypeOf<CircuitBreakerOpenException>());
+        }
+
+        [Then]
+        public void an_open_event_should_be_raised()
+        {
+            OpenedEvent.Verify(e => e.RaiseEvent(_config, BreakerOpenReason.Exception, It.IsAny<Exception>()));
+        }
+
+        [Then]
+        public void two_tollerated_open_events_should_be_raised()
+        {
+            TolleratedOpenEvent.Verify(e => e.RaiseEvent(It.IsAny<short>(), _config, BreakerOpenReason.Exception, It.IsAny<Exception>()), Times.Exactly(2));
+        }
+    }
+}
---
+++ Helpful.CircuitBreaker.Test.Unit/when_executing_code_via_the_breaker/when_tolerating_open_events/when_receiving_exceptions_before_hitting_timeouts_within_tolerance.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using Helpful.BDD;
+using Helpful.CircuitBreaker;
+using Helpful.CircuitBreaker.Exceptions;
+using Helpful.CircuitBreaker.Test.Unit;
+using Moq;
+using NUnit.Framework;
+
+namespace when_executing_code_via_the_breaker.when_tolerating_open_events
+{
+    class when_receiving_exceptions_before_hitting_timeouts_within_tolerance : using_a_mocked_event_factory
+    {
+        private CircuitBreakerConfig _config;
+        private CircuitBreaker _circuitBreaker;
+        private List<Exception> _caughtExceptions;
+        private ArgumentNullException _thrownException;
+
+        protected override void Given()
+        {
+            base.Given();
+            _caughtExceptions = new List<Exception>();
+            _thrownException = new ArgumentNullException();
+            _config = new CircuitBreakerConfig
+            {
+                Timeout = TimeSpan.FromMilliseconds(1000),
+                UseTimeout = true,
+                OpenEventTolerance = 2
+            };
+            _circuitBreaker = Factory.GetBreaker(_config);
+        }
+
+        protected override void When()
+        {
+            CallExecute();
+            CallExecute();
+        }
+
+        private void CallExecute()
+        {
+            try
+            {
+                _circuitBreaker.Execute(() => { throw _thrownException; });
+            }
+            catch (Exception e)
+            {
+                _caughtExceptions.Add(e);
+            }
+        }
+
+        [Then]
+        public void an_exception_should_be_thrown_for_each_call()
+        {
+            Assert.That(_caughtExceptions.Count, Is.EqualTo(2));
+        }
+
+        [Then]
+        public void no_exceptions_should_be_breaker_open_exceptions()
+        {
+            Assert.That(_caughtExceptions, Has.No.TypeOf<CircuitBreakerOpenException>());
+        }
+
+        [Then]
+        public void the_caught_exceptions_should_be_execution_error_exceptions()
+        {
+            Assert.That(_caughtExceptions, Has.No.TypeOf<CircuitBreakerExecutionErrorException>());
+        }
+
+        [Then]
+        public void the_breaker_should_remain_closed()
+        {
+            Assert.That(_circuitBreaker.State, Is.EqualTo(BreakerState.Closed));
+        }
+
+        [Then]
+        public void two_tollerated_open_events_should_be_raised()
+        {
+            TolleratedOpenEvent.Verify(e => e.RaiseEvent(It.IsAny<short>(), _config, BreakerOpenReason.Exception, It.IsAny<Exception>()), Times.Exactly(2));
+        }
+    }
+}
